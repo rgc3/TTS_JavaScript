cc58f852b0961b2abd633cd7443e453dc20388c6
Robert Cunningham <rgc3dev@gmail.com>
2016-02-18 08:40:52 -0500

    classroom JS work

---
+++ hiking.js
@@ -0,0 +1,11 @@
+var todaysTemperature = 80;
+
+if (todaysTemperature > 50 ) {
+   console.log("I'm going hiking!");
+}
+if (todaysTemperature != 50) {
+  console.log("I will not be going to the store because I can only go to the store when the temperature is 50 degrees.")
+}
+if (todaysTemperature == 80 ) {
+  console.log("What a perfect day!")
+}
