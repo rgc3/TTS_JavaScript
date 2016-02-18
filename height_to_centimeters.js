d3b13d963199064ea4963e3dcbc056f8ed88decd
Robert Cunningham <rgc3dev@gmail.com>
2016-02-18 08:17:59 -0500

    first commit beginning JavaScript folder'

---
+++ height_to_centimeters.js
@@ -0,0 +1,3 @@
+var a = prompt("How tall are you in inches?")
+
+console.log(72 * 2.54);
---
+++ imperial_to_metric_height.js
@@ -0,0 +1,11 @@
+var myName = 'John Smith';
+
+var heightInches = 60;
+
+var weightPounds = 120;
+
+var heightCentimeters = heightInches * 2.54;
+
+var weightKilograms = weightPounds * 0.453592;
+
+console.log(myName + ' is ' + heightCentimeters + ' cm and ' + weightKilograms + ' kg.');
